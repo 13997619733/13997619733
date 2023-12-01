6d2573b33e7f67ce11fd255d8517f706192a3d8a
13997619733 <2606945315@qq.com>
2023-12-01 09:30:12 +0800

    2

--- main.cpp
+++ main.cpp
@@ -1,22 +1,12 @@
-<<<<<<< HEAD
 ﻿#include "mainwindow.h"
-=======
-#include "widget.h"
->>>>>>> 33663358ac79f44e00e159d4eb4710e78ac56ea2
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-<<<<<<< HEAD
     MainWindow w;
     w.show();
     return a.exec();
     asd
-=======
-    Widget w;
-    w.show();
-    return a.exec();
->>>>>>> 33663358ac79f44e00e159d4eb4710e78ac56ea2
 }
